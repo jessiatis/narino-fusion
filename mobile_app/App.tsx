25492422953ad0e385cd299fd1f0b47be075554a
Jessica Paola <jpaola1017@gmail.com>
2024-08-25 18:14:28 -0500

    Instalar y configurar react-navigation

--- mobile_app/App.tsx
+++ mobile_app/App.tsx
@@ -1,10 +1,18 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import HomeScreen from './src/screens/HomeScreen'
+import WelcomeScreen from './src/screens/WelcomeScreen'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Welcome" component={WelcomeScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
