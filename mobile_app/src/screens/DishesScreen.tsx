efa7bea89a2b1d0eec68fd3c26cfaae190683fa7
Jessica Paola <jpaola1017@gmail.com>
2024-11-11 17:41:54 -0500

    Añadir sección de filtros horizontales en la lista de platos

--- mobile_app/src/screens/DishesScreen.tsx
+++ mobile_app/src/screens/DishesScreen.tsx
@@ -1,12 +1,20 @@
-import { View, Text, StatusBar, ScrollView } from 'react-native'
+import { View, Text, StatusBar, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import FoodCard from '../components/FoodCard'
 import { heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import InputSearch from '../components/InputSearch'
 import { DISHES } from '../mocks/dishes'
+import { REGIONS } from '../constants'
 
 export default function DishesScreen() {
   const filteredDishes = DISHES
+  const regions = REGIONS.map(({name}) => ({label: `Región ${name}`, actived: false}))
+  const horizontalFilters = [
+    {label: 'Todo', actived: true },
+    {label: 'Favoritos', actived: false },
+    ...regions,
+  ]
+
   return (
     <View className="flex-1 bg-slate-200" style={{ paddingTop: hp(5)}}>
       <StatusBar barStyle="dark-content" translucent backgroundColor="transparent" />
@@ -21,6 +29,21 @@ export default function DishesScreen() {
 
         {/* Campo de búsqueda */}
         <InputSearch />
+
+        {/* Filtros horizontales */}
+        <ScrollView horizontal showsHorizontalScrollIndicator={false} className="mt-5 mb-2 px-3">
+          {horizontalFilters.map(({label, actived}) => (
+            <TouchableOpacity
+              key={label}
+              className="rounded-full px-4 py-2 mr-1"
+              style={{
+                backgroundColor: actived ? 'rgb(96 99 26 / 0.7)' : 'transparent',
+              }}
+            >
+              <Text style={{color: actived ? '#f6ff52' : ''}}>{label}</Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
 
         {/* Lista de platos */}
         <View className="gap-y-2 p-3">
