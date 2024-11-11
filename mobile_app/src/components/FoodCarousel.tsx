e61b4a0e00629af754e96498d7831a3b0a8e24d8
Jessica Paola <jpaola1017@gmail.com>
2024-11-11 16:57:22 -0500

    Cambiar la key a las tarjetas del carrusel

--- mobile_app/src/components/FoodCarousel.tsx
+++ mobile_app/src/components/FoodCarousel.tsx
@@ -9,8 +9,8 @@ interface FoodCarouselProps {
 const FoodCarousel: React.FC<FoodCarouselProps> = ({ data }) => {
   return (
     <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-      {data.map((item, index) => (
-        <TouchableOpacity key={index} className="ml-4">
+      {data.map((item) => (
+        <TouchableOpacity key={item.id} className="ml-4">
           <FoodCard {...item} />
         </TouchableOpacity>
       ))}
