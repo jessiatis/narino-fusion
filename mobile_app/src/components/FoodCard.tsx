4c43f8abb38a8896222c79a0dc239a91fc847124
Jessica Paola <jpaola1017@gmail.com>
2024-11-11 16:58:28 -0500

    Configurar FoodCard para pueda usarse de forma minificada y añadirla a la lista de platos

--- mobile_app/src/components/FoodCard.tsx
+++ mobile_app/src/components/FoodCard.tsx
@@ -3,6 +3,7 @@ import { View, Text, Image, TouchableOpacity, Alert } from 'react-native'
 import { HeartIcon } from 'react-native-heroicons/solid'
 import { CubeIcon, HeartIcon as HeartOutlineIcon } from 'react-native-heroicons/outline'
 import { REGIONS } from '../constants'
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen'
 
 export interface FoodCardProps {
   id: number;
@@ -11,6 +12,8 @@ export interface FoodCardProps {
   dishName: string;
   description: string;
   isFavorite: boolean;
+  minified?: boolean;
+  className?: string;
   onFavoritePress?: () => void;
   onMapPress?: () => void;
   onArPress?: () => void;
@@ -22,23 +25,56 @@ const FoodCard: React.FC<FoodCardProps> = ({
   dishName,
   description,
   isFavorite = false,
+  minified = false,
   onFavoritePress = () => { Alert.alert('[📌 Pendiente: Favorito]') },
   onMapPress = () => { Alert.alert('[📌 Pendiente: Mapa]') },
   onArPress = () => { Alert.alert('[📌 Pendiente: AR]') },
 }) => {
   const region = REGIONS.find(({id})=> id === regionId)!
   return (
-    <View className="relative rounded-2xl overflow-hidden shadow-lg mr-5" style={{ width: 300, height: 370 }}>
+    <View
+      className="relative rounded-2xl overflow-hidden shadow-lg bg-white justify-between items-center "
+      style={{
+        width: minified ? '100%' : 300,
+        height: minified ? 'auto' : 370,
+        padding: minified ? hp(0.7) : 0,
+        flexDirection: minified ? 'row' : 'column',
+      }}
+    >
       {/* Background Image */}
-      <Image source={{ uri: backgroundImg }} className="absolute inset-0 w-full h-full" resizeMode="cover" />
+      <Image
+        className="inset-0"
+        source={{ uri: backgroundImg }}
+        resizeMode="cover"
+        style={{
+          width: minified ? 100 : '100%',
+          borderRadius: minified ? hp(1.5) : 0,
+          height: minified ? 100 : '100%',
+          position: minified ? 'relative' : 'absolute',
+        }}
+      />
 
       {/* Map Thumbnail */}
-      <TouchableOpacity className="absolute top-3 left-3 bg-white rounded-lg shadow-md" style={{ width: 60, height: 60 }} onPress={onMapPress}>
-        <Image source={{ uri: 'https://raw.githubusercontent.com/gist/hepplerj/2f405df580f66923091ad604ccf2fffc/raw/bc7097cf7a072ce0cbd0c5350db33781529e4618/thumbnail.png' }} className="w-full h-full rounded-md" resizeMode="cover" />
-      </TouchableOpacity>
+      {!minified && (
+        <TouchableOpacity
+          className="absolute top-3 left-3 bg-white rounded-lg shadow-md"
+          style={{ width: 60, height: 60 }}
+          onPress={onMapPress}
+        >
+          <Image source={{ uri: 'https://raw.githubusercontent.com/gist/hepplerj/2f405df580f66923091ad604ccf2fffc/raw/bc7097cf7a072ce0cbd0c5350db33781529e4618/thumbnail.png' }} className="w-full h-full rounded-md" resizeMode="cover" />
+        </TouchableOpacity>
+      )}
 
       {/* Information Box */}
-      <View className="absolute bottom-0 left-0 right-0 bg-white p-4 m-3 mr-6 rounded-lg shadow-md flex-row justify-between items-center">
+      <View
+        className="bottom-0 left-0 right-0 bg-white p-4 rounded-lg shadow-md flex-row justify-between items-center"
+        style={{
+          position: minified ? 'static' : 'absolute',
+          shadowColor: minified ? 'transparent' : 'black',
+          margin: minified ? 0 : hp(1.5),
+          marginRight: minified ? 0 : hp(3),
+        }}
+      >
         <View style={{ flex: 1 }}>
           <View className="flex-row items-center gap-1.5 mb-1">
             <View className="w-1.5 h-1.5 rounded-full aspect-square" style={{backgroundColor: region.color}} />
@@ -55,10 +91,12 @@ const FoodCard: React.FC<FoodCardProps> = ({
         </View>
 
         {/* AR Button */}
-        <TouchableOpacity className="bg-primary-600 -mr-8 aspect-square border border-primary-800/50 shadow-lg shadow-primary-700 rounded justify-center items-center px-4 ml-4" onPress={onArPress}>
-          <CubeIcon color="#f6ff52" size={30} />
-          <Text className="text-[#f6ff52] font-bold">3D</Text>
-        </TouchableOpacity>
+        {!minified && (
+          <TouchableOpacity className="bg-primary-600 -mr-8 aspect-square border border-primary-800/50 shadow-lg shadow-primary-700 rounded justify-center items-center px-4 ml-4" onPress={onArPress}>
+            <CubeIcon color="#f6ff52" size={30} />
+            <Text className="text-[#f6ff52] font-bold">3D</Text>
+          </TouchableOpacity>
+        )}
       </View>
 
       {/* Favorite Button */}
@@ -66,8 +104,8 @@ const FoodCard: React.FC<FoodCardProps> = ({
         <Text className="aspect-square text-center">
           {
             isFavorite
-            ? (<HeartIcon color="#f6ff52" size={30} />)
-            : (<HeartOutlineIcon color="#f6ff52" size={30} />)
+            ? (<HeartIcon color="#f6ff52" size={minified ? 20 : 30 } />)
+            : (<HeartOutlineIcon color="#f6ff52" size={minified ? 20 : 30} />)
           }
         </Text>
       </TouchableOpacity>
--- mobile_app/src/screens/DishesScreen.tsx
+++ mobile_app/src/screens/DishesScreen.tsx
@@ -1,10 +1,83 @@
-import { View, Text } from 'react-native'
+import { View, Text, StatusBar, ScrollView } from 'react-native'
 import React from 'react'
+import FoodCard from '../components/FoodCard'
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen'
+import InputSearch from '../components/InputSearch'
 
 export default function DishesScreen() {
+  const dishes = [
+    {
+      id: 1,
+      backgroundImg: 'https://narinofusion.co/wp-content/uploads/2024/09/7-1536x799.jpeg',
+      regionId: 1,
+      dishName: 'Pan de Maíz',
+      description: 'Vereda Tola de las Lajas',
+      isFavorite: false,
+    },
+    {
+      id: 2,
+      backgroundImg: 'https://narinofusion.co/wp-content/uploads/2022/07/subscribe-form-background.webp',
+      regionId: 2,
+      dishName: 'Arepa de Huevo',
+      description: 'Costa Atlántica',
+      isFavorite: true,
+    },
+    {
+      id: 3,
+      backgroundImg: 'https://narinofusion.co/wp-content/uploads/2024/09/Inicio-1536x1024.jpg',
+      regionId: 3,
+      dishName: 'Sancocho de Pescado',
+      description: 'Buenaventura',
+      isFavorite: false,
+    },
+    {
+      id: 4,
+      backgroundImg: 'https://narinofusion.co/wp-content/uploads/2024/07/Lapingachos-home-1536x858.webp',
+      regionId: 3,
+      dishName: 'Pirarucú Asado',
+      description: 'Leticia',
+      isFavorite: true,
+    },
+    {
+      id: 5,
+      backgroundImg: 'https://narinofusion.co/wp-content/uploads/2024/09/6.jpeg',
+      regionId: 1,
+      dishName: 'Mamona',
+      description: 'Villavicencio',
+      isFavorite: false,
+    },
+  ]
+
   return (
-    <View>
-      <Text>DishesScreen</Text>
+    <View className="flex-1 bg-slate-200" style={{ paddingTop: hp(5)}}>
+      <StatusBar barStyle="dark-content" translucent backgroundColor="transparent" />
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        className="mt-10"
+      >
+        <Text className="text-primary-800 font-light text-3xl text-center mb-3">
+          Lista de platos
+        </Text>
+
+        {/* Campo de búsqueda */}
+        <InputSearch />
+
+        {/* Lista de platos */}
+        <View className="gap-y-2 p-3">
+          {dishes.map((dish)=>(
+            <View key={dish.id}>
+              <FoodCard minified {...dish} />
+            </View>
+          ))}
+        </View>
+
+        <View className="pb-28 pt-5">
+          <Text className="text-center text-slate-500">
+            ¡Has llegado al final!
+          </Text>
+        </View>
+      </ScrollView>
     </View>
   )
 }
