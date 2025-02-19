b7b648132fdaf876062215d4a79150bb98a0fac9
Jessica Paola <jpaola1017@gmail.com>
2025-02-19 18:51:12 -0500

    Añadir traducciones a la pantalla 'Regiones'

--- mobile_app/src/screens/RegionsScreen.tsx
+++ mobile_app/src/screens/RegionsScreen.tsx
@@ -2,18 +2,21 @@ import React from 'react'
 import { StatusBar, Text, View } from 'react-native'
 import { heightPercentageToDP, heightPercentageToDP as hp, widthPercentageToDP } from 'react-native-responsive-screen'
 import RegionsMap from '../components/RegionsMap'
+import { useTranslation } from 'react-i18next'
 
 export default function RegionsScreen() {
+  const { t } = useTranslation()
+
   return (
     <View className="flex-1 bg-slate-200" style={{ paddingTop: hp(2)}}>
       <StatusBar barStyle="dark-content" translucent backgroundColor="transparent" />
 
         <Text className="text-primary-900 font-light text-3xl text-center mb-2 mt-16">
-          Regiones
+          {t('regions.title')}
         </Text>
 
         <Text className="text-primary-900 font-light text-xl text-center mb-3 px-6">
-          Selecciona una región para ver los platos típicos de la zona:
+          {t('regions.description')}
         </Text>
 
         <View className="flex-1 justify-start items-center py-10">
