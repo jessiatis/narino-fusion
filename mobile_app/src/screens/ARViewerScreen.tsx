ec3628882502e35548a4cdba33b3ef4a6f5a9979
Jessica Paola <jpaola1017@gmail.com>
2025-02-20 15:33:27 -0500

    Añadir botón de ir atrás en el visualizador de realidad aumentada

--- mobile_app/src/screens/ARViewerScreen.tsx
+++ mobile_app/src/screens/ARViewerScreen.tsx
@@ -1,14 +1,26 @@
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
 import { ArViewerView } from 'react-native-ar-viewer';
 import { DishType } from '../types';
+import { useNavigation } from '@react-navigation/native';
+import { ArrowLeftIcon } from 'react-native-heroicons/outline';
+import { COLORS } from '../constants/theme';
 
 type Props = { route?: { params: { dish: DishType } } }
 
 export default function ARViewerScreen({ route }: Props) {
+  const navigation = useNavigation()
   const { dish } = route!.params
   return (
     <View style={styles.container}>
+      {/* Botón de ir atrás */}
+      <TouchableOpacity
+        className="w-auto self-start p-3 bg-primary-600/80 rounded-full absolute top-16 left-4 z-50"
+        onPress={() => navigation.goBack()}
+      >
+        <ArrowLeftIcon size={24} strokeWidth={1.8} color={COLORS.accent} />
+      </TouchableOpacity>
+
       <ArViewerView
         model={`file:///android_asset/custom/${dish["3DModel"]}`}
         style={styles.arView}
@@ -17,6 +29,7 @@ export default function ARViewerScreen({ route }: Props) {
         allowRotate
         allowScale
         allowTranslate
+        planeOrientation='horizontal'
         onStarted={() => console.log('Iniciado')}
         onEnded={() => console.log('Finalizado')}
         onModelPlaced={() => console.log('Modelo mostrado')}
