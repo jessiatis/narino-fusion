d0064e98edc20ea58eb16fc769cba1a55e6c9672
soyluisarrieta <luisarrieta796@gmail.com>
2025-02-27 16:48:20 -0500

    Actualizar imágenes de cada región

--- mobile_app/src/constants/regions.ts
+++ mobile_app/src/constants/regions.ts
@@ -4,20 +4,20 @@ export const REGIONS = [
     name: 'regions.pacifica.name',
     color: '#f0c200',
     description: 'regions.pacifica.description',
-    image: 'https://narinofusion.co/wp-content/uploads/2022/07/menu-heading-background-1.webp',
+    image: 'https://narinofusion.co/wp-content/uploads/2025/02/REGION-PACIFICA.webp',
   },
   {
     id: 2,
     name: 'regions.andina.name',
     color: '#078930',
     description: 'regions.andina.description',
-    image: 'https://narinofusion.co/wp-content/uploads/2022/07/menu-heading-background-2.webp',
+    image: 'https://narinofusion.co/wp-content/uploads/2025/02/REGION-ANDINA.webp',
   },
   {
     id: 3,
     name: 'regions.amazonia.name',
     color: '#ff7f00',
     description: 'regions.amazonia.description',
-    image: 'https://narinofusion.co/wp-content/uploads/2022/07/menu-heading-background-3.webp',
+    image: 'https://narinofusion.co/wp-content/uploads/2025/02/REGION-AMAZONICA.webp',
   },
 ]
